132334b827d474ba63fe6bea1b7fc21586755e87
hernanjkd <hernanjkd@gmail.com>
2019-08-02 21:31:39 -0400

    fixing app

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const fetch = require("node-fetch");
 const { studentCleanup } = require("./formater.js");
 
+
+const access_token = "76dd7a6f655407ac1a65b2b9c52aa8a10fe4939e";
+const availableEndPoints = ["students", "user"];
+const get = availableEndPoints[0];
+
 const url = `https://api.breatheco.de/${get}/?access_token=${access_token}`;
 const resp = await fetch(url);
 const data = await resp.json();
