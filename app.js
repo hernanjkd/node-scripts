bf695153c349826a91662fbec9595c1ec7497fc3
hernanjkd <hernanjkd@gmail.com>
2019-08-05 15:52:52 -0400

    validation

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const access_token = "76dd7a6f655407ac1a65b2b9c52aa8a10fe4939e";
 const availableEndPoints = ["students", "user"];
 const get = availableEndPoints[0];
 
-const url = `https://api.breatheco.de/${get}/?access_token=${access_token}`;
+const url = (id) => `https://api.breatheco.de/${get}/${id}?access_token=${access_token}`;
 const resp = await fetch(url);
 const data = await resp.json();
 
 
-const updateStudent = (student) => fetch('',{
+const updateStudent = (student) => fetch(url(student.id),{
     method: 'PUT',
     headers: {
         "Content-Type": "application/json"
@@ -24,12 +24,17 @@ const updateStudent = (student) => fetch('',{
 
 const cleanedStudents = studentCleanup(data);
 
-for (let i in cleanedStudents)
+
+let i = cleanedStudents.length;
+const next = () => {
     setTimeOut(() => {
         console.log(`"${data[i].first_name}, ${data[i].last_name}" - ${data.email}`);
         console.log(`"${cleanedStudents[i].first_name}, ${cleanedStudents[i].last_name}"`);
         console.log('');
         await updateStudent(cleanedStudents[i]);
+        i && next();
     }, 1000);
+}
+next();
 
 console.log('Complete!');
