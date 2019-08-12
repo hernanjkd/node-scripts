a6815ceb85a6e4fa042e2fb32b5570441fa2bbc9
hernanjkd <hernanjkd@gmail.com>
2019-08-11 23:37:22 -0400

    code almost ready

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const { studentCleanup } = require("./formatter.js");
 const access_token = "9c7b0fb28dba87a165b52e8d77597fc6ecd89707";
 const get = {students: "students", user: "user"};
 
+const url = (id) => `https://api.breatheco.de/${get.students}/${id}?access_token=${access_token}`;
+
 async function asyncFetch(_url, _opt) {
     const response = await fetch(_url, _opt);
     const data = await response.json();
     return json;
 }
-
-const url = (id) => `https://api.breatheco.de/${get.students}/${id}?access_token=${access_token}`;
 
 const updateStudent = (student) => await asyncFetch(url(student.id),{
     method: 'PUT',
@@ -21,14 +21,10 @@ const updateStudent = (student) => await asyncFetch(url(student.id),{
     body: JSON.stringify(student)
 });
 
-
-let data = asyncFetch(url());
-
-const cleanedStudents = studentCleanup(data);
-
 let i = cleanedStudents.length;
 const next = () => {
     setTimeOut(() => {
+        i--;
         console.log(`"${data[i].first_name}, ${data[i].last_name}" - ${data.email}`);
         console.log(`"${cleanedStudents[i].first_name}, ${cleanedStudents[i].last_name}"`);
         console.log('');
@@ -36,6 +32,12 @@ const next = () => {
         i && next();
     }, 1000);
 }
+
+
+let { data } = asyncFetch(url());
+
+const cleanedStudents = studentCleanup(data);
+
 next();
 
 console.log('Complete!');
