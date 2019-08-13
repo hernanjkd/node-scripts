e5cb84c3b9f194ece77b3533010bff27e90d998b
hernanjkd <hernanjkd@gmail.com>
2019-08-13 09:12:14 -0400

    fetch will always be asynchronous

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const fetch = require("node-fetch");
 const { studentCleanup } = require("./formatter.js");
 
 
-const access_token = "9c7b0fb28dba87a165b52e8d77597fc6ecd89707";
+const access_token = "b86ee02825166758efd20ceb275c5cdca49e4d1b";
 const get = {students: "students", user: "user"};
 
 const url = (id='') => `https://api.breatheco.de/${get.students}/${id}?access_token=${access_token}`;
 
 async function asyncFetch(_url, _opt={}) {
-    const response = await fetch(_url, _opt);
-    const data = await response.json();
+    const resp = await fetch(_url, _opt);
+    const data = await resp.json();
     return data;
 }
 
@@ -22,13 +22,13 @@ const updateStudent = (student) => asyncFetch(url(student.id),{
 });
 
 
-let { data } = asyncFetch(url());
-console.log(asyncFetch(url()));
-//const cleanStudents = studentCleanup(data);
+let data = asyncFetch(url());
+
+const cleanStudents = studentCleanup(data);
 
 let i = cleanStudents.length;
 const next = () => {
-    setTimeOut(() => {
+    setTimeout(() => {
         i--;
         console.log(`"${data[i].first_name}, ${data[i].last_name}" - ${data[i].email}`);
         console.log(`"${cleanStudents[i].first_name}, ${cleanStudents[i].last_name}"`);
@@ -40,3 +40,28 @@ const next = () => {
 next();
 
 console.log('Complete!');
+
+// async function asyncFetch() {
+//     // let resp = await fetch('https://api.breatheco.de/students/?access_token=b86ee02825166758efd20ceb275c5cdca49e4d1b')
+//     let resp = await fetch('http://127.0.0.1:3000/');
+//     let data = await resp.json();
+//     return data;
+// }
+
+// let x = async () => {
+//     let data = await asyncFetch();
+//     console.log(data);
+// }
+// x();
+
+
+// fetch('http://127.0.0.1:3000/')
+// .then(resp => resp.json())
+// .then(data => console.log(data));
+
+
+// console.log(data.code)
+// console.log( Array.isArray(data.data) )
+// setTimeout(() => {
+// console.log( data );
+// }, 3000);
