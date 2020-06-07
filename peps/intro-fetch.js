133ca044fb45806d72dc19e88a371c44f3905381
hernanjkd <hernanjkd@gmail.com>
2020-06-06 23:52:40 -0400

    testing

--- peps/intro-fetch.js
+++ peps/intro-fetch.js
@@ -3,5 +3,9 @@ const fetch = require('node-fetch')
 const url = 'https://swapi.dev/api/planets/'
 
 fetch(url)
-    .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(response => response.json())
+    .then(info => {
+
+    })
+
+
