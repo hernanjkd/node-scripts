302bc7b50efc81e0e2fd7934a0a1e9dcdf4310a9
hernanjkd <hernanjkd@gmail.com>
2020-06-07 00:28:30 -0400

    while fetch

--- peps/intro-fetch.js
+++ peps/intro-fetch.js
@@ -2,10 +2,32 @@ const fetch = require('node-fetch')
 
 const url = 'https://swapi.dev/api/planets/'
 
-fetch(url)
-    .then(response => response.json())
-    .then(info => {
-
+function fetchSwapi(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data => {
+                resolve(data)
+            })
+            .catch(err => reject(err))
     })
 
+}
 
+// fetchSwapi(url).then(data => console.log(data))
+
+async function test() {
+    let more = true
+    let urls = [url]
+    while (more) {
+        await fetch(urls[urls.length - 1])
+            .then(resp => resp.json())
+            .then(data => {
+                more = data.next ? true : false
+                urls.push(data.next)
+            })
+    }
+    console.log(urls)
+}
+
+test()
