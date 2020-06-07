f6f4d92f2da5bbd912bf0f7092901c03e7bb5d73
hernanjkd <hernanjkd@gmail.com>
2020-06-07 00:45:43 -0400

    async await fetch

--- peps/intro-fetch.js
+++ peps/intro-fetch.js
@@ -2,29 +2,58 @@ const fetch = require('node-fetch')
 
 const url = 'https://swapi.dev/api/planets/'
 
-function fetchSwapi(url) {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(resp => resp.json())
-            .then(data => {
-                resolve(data)
-            })
-            .catch(err => reject(err))
-    })
+// function fetchSwapi(url) {
+//     return new Promise((resolve, reject) => {
+//         fetch(url)
+//             .then(resp => resp.json())
+//             .then(data => {
+//                 resolve(data)
+//             })
+//             .catch(err => reject(err))
+//     })
 
-}
+// }
 
 // fetchSwapi(url).then(data => console.log(data))
 
-async function test() {
-    let more = true
+
+/***********************
+ *  NOT WAITING FOR IT
+ ***********************/
+function test() {
+    let next = true
     let urls = [url]
-    while (more) {
+    let count = 0
+    while (next && count < 100) {
+        console.log('count', ++count)
+        fetch(urls[urls.length - 1])
+            .then(resp => resp.json())
+            .then(data => {
+                console.log('fetch')
+                next = data.next ? true : false
+                next && urls.push(data.next)
+            })
+    }
+    console.log(urls)
+}
+
+test()
+
+/********************************
+ *  WAITING FOR THE ASYNC FETCH
+ ********************************/
+async function test() {
+    let next = true
+    let urls = [url]
+    let count = 0
+    while (next && count < 100) {
+        console.log('count', ++count)
         await fetch(urls[urls.length - 1])
             .then(resp => resp.json())
             .then(data => {
-                more = data.next ? true : false
-                urls.push(data.next)
+                console.log('fetch')
+                next = data.next ? true : false
+                next && urls.push(data.next)
             })
     }
     console.log(urls)
