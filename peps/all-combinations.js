e78e2bf21705fff2bce725d2a6ffa809fe2ba157
hernanjkd <hernanjkd@gmail.com>
2020-06-25 02:31:35 -0400

    algorithms

--- peps/all-combinations.js
+++ peps/all-combinations.js
@@ -5,9 +5,40 @@ const lock = [
     ['*', '0', '#']
 ]
 
+let coordinates = {
+    '1': ['2', '4'],
+    '2': ['1', '5', '3']
+}
+
 function getPins(pin) {
-    if (pin.length < 1 || pin.length > 8)
+    if (isNaN(pin) || pin.length < 1 || pin.length > 8)
         return 'Invalid pin'
 
-    let allVariations
+    let allVariations = []
+    for (let e of pin) {
+        let temp = [e]
+
+    }
 }
+
+function createCoordinates() {
+    let coords = {}
+    for (let i in lock)
+        for (let j in lock[i]) {
+            coords[lock[i][j]] = getSurroundings(Number(i), Number(j))
+        }
+}
+
+function getSurroundings(i, j) {
+    let mem = []
+    if (lock[i + 1] !== undefined && !isNaN(lock[i + 1][j]))
+        mem.push(lock[i + 1][j])
+    if (lock[i - 1] !== undefined && !isNaN(lock[i - 1][j]))
+        mem.push(lock[i - 1][j])
+    if (lock[i])
+
+}
+
+// let i = 2; let j = 3;
+// [i, j] = [i + 1, j + 1]
+// console.log(i, j)
