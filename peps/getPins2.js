7eba5e0119219d5f297dce708d59aee6d3401bdf
hernanjkd <hernanjkd@gmail.com>
2020-06-30 22:03:37 -0400

    dynamic loops

---
+++ peps/getPins2.js
@@ -0,0 +1,8 @@
+let who = ['John', 'Peps']
+let what = [' jumps ', ' codes ']
+let when = ['in the afternoon.', 'at night.']
+let why = ['1', '2']
+
+let all = [who, what, when, why]
+
+function loop(arr, )
