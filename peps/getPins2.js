02f057a1dd435913fae3b9c99a9e1e296ab79aea
hernanjkd <hernanjkd@gmail.com>
2020-06-30 22:06:57 -0400

    recursive loop

--- peps/getPins2.js
+++ peps/getPins2.js
@@ -1,8 +1,16 @@
 let who = ['John', 'Peps']
 let what = [' jumps ', ' codes ']
 let when = ['in the afternoon.', 'at night.']
-let why = ['1', '2']
+
 
 let all = [who, what, when, why]
 
-function loop(arr, )
+function loop(arr, vars) {
+    if (arr.length === 0)
+        console.log(vars)
+    else {
+        let a = [...arr]
+        for (let e of a.shift())
+            loop(a, vars + e)
+    }
+}
